3cea425cc8cb518d56daab3fdebfe3c3a890f609
Brian Wong <brwwong@cs.stonybrook.edu>
2018-04-17 23:16:25 -0700

    wrote filelistinit() and cse320_fopen(), have not tested yet

--- cse320_functions.h
+++ cse320_functions.h
@@ -3,7 +3,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
-
+#include <string.h>
 int addr_count=0;
 int file_count=0;
 struct addr_in_use{
@@ -21,6 +21,7 @@ struct files_in_use{
 };
 
 struct addr_in_use addr_list[25];
+struct files_in_use file_list[25];
 
 void init_addr_list(){
 	int i=0;
@@ -32,11 +33,26 @@ void init_addr_list(){
 		i++;
 	}	
 }
+
+void init_file_list(){
+	int i=0;
+	while(i<25){
+		struct files_in_use new_file;
+		new_file.filename=NULL;
+		new_file.ref_count=0;
+		file_list[i]=new_file;
+		i++;
+	}	
+}
 void* cse320_malloc(size_t size){
 	void* ptr=NULL;
 	//do stuff with global list
 	//iterate through list, find first free one
 	int i=0;
+	if (addr_count>=25){
+		errno=ENOMEM;
+		exit(-1);
+	}
 	while(i<25){
 		if(addr_list[i].ref_count==0){
 			ptr=malloc(size);
@@ -62,6 +78,7 @@ void cse320_free(void* ptr){
 			else{
 				free(ptr);
 				addr_list[i].ref_count--;	
+				addr_count--;
 				return;
 			}
 		}
@@ -72,7 +89,24 @@ void cse320_free(void* ptr){
 	exit(-1);
 	
 }
-void* cse320_fopen(){
+FILE* cse320_fopen(const char *pathname, const char *mode){
+	FILE* fp=NULL;
+	int i =0;
+	if(file_count>=25){
+		errno=ENFILE;
+		exit(-1);
+	}
+	fp=fopen(pathname,mode);
+	while(i<25){
+		//check to see if the file descriptor already exists
+		if(strcmp(file_list[i].filename,pathname)==0){
+			file_list[i].ref_count++;
+			return fp;
+		}
+
+	}
+	file_count++;
+	return fp;
 }
 void cse320_fclose(){
 }
